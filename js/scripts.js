da66190b2941b08846d424ccfcd78aaa808d1869
Bryan Cable <bryancable@Bryans-MacBook-Pro.local>
2018-04-10 08:32:38 -0700

    added var myHobby

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,8 @@ var myHistory = "I want to make a living doing something I enjoy.";
 console.log(myHistory);
 var myValue = confirm(myHistory);
 console.log(myValue);
+var myHobby = "I love playing tennis";
+console.log(myHobby)
+var myHobbyValue = confirm(myHobby);
+console.log(myHobbyValue);
 
