cde1eec3bf836d01a9409348ab65776cbe7807e2
Bryan Cable <bryancable@Bryans-MacBook-Pro.local>
2018-04-12 14:46:21 -0700

    class notes

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+console.log(myName);
+var myName= 30;
+myName = new Number(myName);
+console.log(typeof(myName));
+
+
+var myName = "Bryan";
+
+console.log(myName.toUpperCase());
+
+console.log(myName, "one");
+{
+    var myName = "Cable";
+    console.log(myName, "two");
+};
+console.log(myName, "three");
+
+let birthday = '10/19/87';
+let rightNow = new Date (birthday);
+
+alert(rightNow);
+
+// alert('Hey, you forgot your socks')
+
+// confirm("Are you 21 or older");
+
+// PROMPT
+const userStory = prompt('Tell me, do you take walks');
+
+console.log(userStory);
+
